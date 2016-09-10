30368e641608383e6c98d95bf730c0744e7510f3
FroZnShiva <froznshiva@gmx.net>
2016-09-10 17:32:27 +0200

    Update styles
    Used base16-builder-textual@0a5d8f4e1786c7016e220f2da57f0744d4ca79a4.

--- styles/Base16 Bespin Dark/scripts.js
+++ styles/Base16 Bespin Dark/scripts.js
@@ -1,85 +1,188 @@
+var enableGrouping = false;
+var expandNewGroups = false;
+var groupingInclude = undefined;
+var groupingExclude = undefined;
 /* Defined in: "Textual 5.app -> Contents -> Resources -> JavaScript -> API -> core.js" */
 
-var mappedSelectedUsers = new Array();
+var groupCount = 0
+var currentChannel = undefined
+
+Textual.viewInitiated = function(viewType, serverHash, channelHash, channelName)
+{
+	currentChannel = (viewType == 'channel') ? channelName : undefined
+}
 
 Textual.viewBodyDidLoad = function()
 {
-	Textual.fadeOutLoadingScreen(1.00, 0.95);
+	Textual.fadeOutLoadingScreen(1.00, 0.95)
 
 	setTimeout(function() {
 		Textual.scrollToBottomOfView()
-	}, 500);
+	}, 500)
 }
 
 Textual.newMessagePostedToView = function(line)
 {
-	var element = document.getElementById("line-" + line);
+	var element = document.getElementById("line-" + line)
 
-	updateNicknameAssociatedWithNewMessage(element);
+	ConversationTracking.updateNicknameWithNewMessage(element)
+
+	if (enableGrouping && currentChannel) {
+		if (groupingInclude) {
+			enableGrouping = groupingInclude.indexOf(currentChannel) != -1
+		} else if (groupingExclude) {
+			enableGrouping = groupingExclude.indexOf(currentChannel) == -1
+		}
+	}
+
+	if (enableGrouping) {
+		groupGeneralUserEvents(element)
+	}
 }
 
 Textual.nicknameSingleClicked = function(e)
 {
-	userNicknameSingleClickEvent(e);
+	ConversationTracking.nicknameSingleClickEventCallback(e)
 }
 
-function updateNicknameAssociatedWithNewMessage(e)
+function isGeneralUserEvent(element)
 {
-	/* We only want to target plain text messages. */
-	var elementType = e.getAttribute("ltype");
-
-	if (elementType == "privmsg" || elementType == "action") {
-		/* Get the nickname information. */
-		var senderSelector = e.querySelector(".sender");
-
-		if (senderSelector) {
-			/* Is this a mapped user? */
-			var nickname = senderSelector.getAttribute("nickname");
-
-			/* If mapped, toggle status on for new message. */
-			if (mappedSelectedUsers.indexOf(nickname) > -1) {
-				toggleSelectionStatusForNicknameInsideElement(senderSelector);
-			}
-		}
-	}
+	var type = element.getAttribute("command")
+	return ["join", "part", "quit", "nick"].indexOf(type) != -1
 }
 
-function toggleSelectionStatusForNicknameInsideElement(e)
+function isGroup(element)
 {
-	/* e is nested as the .sender so we have to go three parents
-	 up in order to reach the parent div that owns it. */
-	var parentSelector = e.parentNode.parentNode.parentNode.parentNode;
-
-	parentSelector.classList.toggle("selectedUser");
+	var type = element.getAttribute("ltype")
+	return type == "group"
 }
 
-function userNicknameSingleClickEvent(e)
+function isMarker(element)
 {
-	/* This is called when the .sender is clicked. */
-	var nickname = e.getAttribute("nickname");
+	return element && element.id && element.id == 'mark'
+}
 
-	/* Toggle mapped status for nickname. */
-	var mappedIndex = mappedSelectedUsers.indexOf(nickname);
+function groupGeneralUserEvents(currentLine)
+{
+	var previousLine = currentLine.previousElementSibling
 
-	if (mappedIndex == -1) {
-		mappedSelectedUsers.push(nickname);
-	} else {
-		mappedSelectedUsers.splice(mappedIndex, 1);
+	// Ignore marker line
+	if (isMarker(previousLine))
+		previousLine = previousLine.previousElementSibling
+
+	if (!isGeneralUserEvent(currentLine) || !previousLine)
+		return
+
+	var previousType = previousLine.getAttribute("ltype")
+
+	// Don't group if there is just one line.
+	if (!(isGeneralUserEvent(previousLine) || isGroup(previousLine)))
+		return
+
+	var group = previousLine
+
+	if (!isGroup(previousLine) && previousType != "general_user_events") {
+		group = createGroup("general_user_events")
+		currentLine.parentElement.insertBefore(group, currentLine)
 	}
 
-	/* Gather basic information. */
-	var documentBody = document.getElementById("body_home");
+	if (!isGroup(previousLine)) {
+		increaseAttribute(group, previousType + "s")
+		addLineToGroup(group, previousLine)
+	}
 
-	var allLines = documentBody.querySelectorAll('div[ltype="privmsg"], div[ltype="action"]');
+	// Increase before decorating, else the numbers might be off.
+	var currentType = currentLine.getAttribute("command")
+	increaseAttribute(group, currentType + "s")
 
-	/* Update all elements of the DOM matching conditions. */
-	for (var i = 0, len = allLines.length; i < len; i++) {
-		var sender = allLines[i].querySelectorAll(".sender");
+	addLineToGroup(group, currentLine)
+}
 
-		if (sender.length > 0) {
-			if (sender[0].getAttribute("nickname") === nickname) {
-				toggleSelectionStatusForNicknameInsideElement(sender[0]);
-			}
+function createGroup(name)
+{
+	var group = document.createElement("details")
+	group.id = "group-" + groupCount
+	group.setAttribute("ltype", "group")
+	group.setAttribute("class", "line group")
+	group.setAttribute("gtype", name)
+
+	if (expandNewGroups) {
+		group.setAttribute("open", "")
+	}
+
+	var head = document.createElement("summary")
+	head.id = "group_head-" + groupCount
+	group.appendChild(head)
+
+	var body = document.createElement("div")
+	body.id = "group_body-" + groupCount
+	body.setAttribute("class", "group_body")
+	group.appendChild(body)
+
+	var time = document.createElement("span")
+	time.setAttribute("class", "time")
+	head.appendChild(time)
+
+	var message = document.createElement("span")
+	message.setAttribute("class", "message")
+	message.appendChild(document.createTextNode(""))
+	head.appendChild(message)
+
+	groupCount++
+
+	return group
+}
+
+function addLineToGroup(group, line)
+{
+	line.parentNode.removeChild(line)
+	group.lastElementChild.appendChild(line)
+
+	var head = group.firstElementChild
+	var time = line.firstElementChild.firstElementChild.cloneNode(true)
+
+	head.removeChild(head.firstElementChild)
+	head.insertBefore(time, head.firstElementChild)
+
+	decorateGroup(group)
+}
+
+function increaseAttribute(element, name)
+{
+	var value = Number(element.getAttribute(name))
+	element.setAttribute(name, value + 1)
+}
+
+function decorateGroup(group)
+{
+	function stringifyValue(name, title)
+	{
+		if (title == undefined)
+			title = name
+
+		var value = Number(group.getAttribute(name + "s"))
+		if (value == 0)
+			return undefined
+
+		if (title == "nick")
+			title = "nick change"
+
+		return value + " " + (value == 1 ? title : title + "s")
+	}
+
+	var gtype = group.getAttribute("gtype")
+	var head = group.firstElementChild
+	var message = head.lastElementChild
+	var text = message.firstChild
+
+	if (gtype == "general_user_events") {
+		types = ["join", "part", "quit", "nick"]
+		contents = new Array()
+		for (var i in types) {
+			var value = stringifyValue(types[i])
+			if (value)
+				contents.push(value)
 		}
+		text.textContent = " " + contents.join(", ")
 	}
 }
